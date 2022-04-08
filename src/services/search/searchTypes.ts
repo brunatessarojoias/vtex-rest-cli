fd5fd85cff0fc1ccc460c77b609cb3620c8624ce
Augusto Gallo <finardiaugusto@gmail.com>
2022-04-08 17:13:59 -0300

    update searchTypes

--- src/services/search/searchTypes.ts
+++ src/services/search/searchTypes.ts
@@ -1,39 +1,38 @@
+export interface SearchOptions {
+	filters?: SearchFilters;
+	sorting?: SearchSorting;
+	pagination?: SearchPagination;
+}
+
 export interface SearchFilters {
-	brand: string;
-	category: string;
-	collectionId: number;
-	ean: string;
-	fullText: string;
-	priceRange: {
+	brand?: string;
+	category?: number;
+	collectionId?: number;
+	ean?: string;
+	fullText?: string;
+	priceRange?: {
 		from: number;
 		to: number;
 	};
-	productId: number;
-	referenceId: number;
-	salesChannel: Record<number, boolean>;
-	seller: number;
-	skuId: number;
-	specification: {
-		id: number;
-		value: string;
-	};
+	productId?: number;
+	referenceId?: number;
+	salesChannel?: Array<[number, boolean]>;
+	seller?: number;
+	skuId?: number;
+	specification?: Array<[number, string]>;
+}
+
+export interface SearchSorting {
+	bestDiscounts?: string;
+	bestReviews?: string;
+	name?: string;
+	price?: string;
+	releaseDate?: string;
+	score?: string;
+	topSelling?: string;
 }
 
 export interface SearchPagination {
-	from: number;
-	to: number;
-}
-
-type SortAscending = "ASC";
-type SortDescending = "DESC";
-type SortingOrder = SortAscending | SortDescending;
-
-export interface SearchSorting {
-	bestDiscounts: SortDescending;
-	bestReviews: SortDescending;
-	name: SortingOrder;
-	price: SortingOrder;
-	releaseDate: SortDescending;
-	score: SortDescending;
-	topSelling: SortDescending;
+	from?: number;
+	to?: number;
 }
