b0524885f059b8dfb2d46d0d92b5d403518f7797
Augusto Gallo <finardiaugusto@gmail.com>
2022-04-13 17:16:24 -0300

    add TODO to 'paramsSerializer.ts' about building query string with repeated parameters

--- src/config/api/utils/paramsSerializer.ts
+++ src/config/api/utils/paramsSerializer.ts
@@ -1,6 +1,8 @@
 type ParameterValue = string | number;
 type ParametersObject = Record<string, ParameterValue>;
 
+// TODO - Handle repeated parameters
+// ? https://stackoverflow.com/questions/42898009/multiple-fields-with-same-key-in-query-params-axios-request
 export default function paramsSerializer(params: string | ParametersObject) {
 	if (typeof params === "string") {
 		return encodeURI(params);
